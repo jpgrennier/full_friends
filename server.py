2003a961e756531581d2eaab87dd3cf7851d6446
Joseph Grennier <jp_grennier@yahoo.com>
2015-11-09 21:57:10 -0800

    It's allgood, time for bed

--- server.py
+++ server.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
-app.secret_key = 'wordsAndStuffandThings'
+# app.secret_key = 'wordsAndStuffandThings' *ONLY REQ FOR SESSIONS*
 mysql = MySQLConnector('friendsdb')
 
-#---------------------------------------
-# INDEX DONE 
 @app.route('/')
 def index():
     friends = mysql.fetch("SELECT * FROM friends")
     return render_template('index.html', friends=friends)
 
-# CREATE DONE
 @app.route('/friends', methods=['POST'])
 def create():
     query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES ('{}', '{}', '{}', NOW(), NOW())".format(request.form['first_name'], request.form['last_name'], request.form['occupation'])
@@ -19,23 +16,94 @@ def create():
     mysql.run_mysql_query(query)
     return redirect('/')
 
-#---------------------------------------
-
-@app.route('/friends/<id>/edit', methods=['GET'])
+@app.route('/friends/<int:id>/edit', methods=['GET']) #int: not necessary, but good to know
 def edit(id):
+	friend = mysql.fetch("SELECT * FROM friends where id = {}".format(id))
+	if friend:
+		return render_template('edit.html', friends = friend)
+	return redirect('/friends/'+str(id))
+	# return edit
 
-	# function goes here 
-	return edit
-
-@app.route('/friends/<id>', methods=['POST'])
+@app.route('/friends/<int:id>/update', methods=['POST']) #int: not necessary, but good to know
 def update(id):
-	# function goes here
-	return update
+	query = "UPDATE friends SET first_name='{}', last_name='{}', occupation='{}' WHERE id='{}'".format(request.form['first_name'], request.form['last_name'], request.form['occupation'],id)
+	# print query
+	mysql.run_mysql_query(query)
+	return redirect('/')
+	# return update
 
-@app.route('/friends/<id>/delete', methods=['POST'])
-def destroy(id):
-	# DELETE * from where id =
-	# function goes here
-	return destroy
+@app.route('/friends/<int:id>/delete', methods = ['POST']) #int: not necessary, but good to know
+def delete(id):
+	friend = mysql.fetch("SELECT * FROM friends where id ={}".format(id))
+	query = "DELETE FROM friendsdb.friends WHERE id={}".format(id)
+	mysql.run_mysql_query(query)
+	return redirect('/')
 
 app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
